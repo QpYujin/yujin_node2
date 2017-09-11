6f1215e5aeb7edc72802f3bbe0b89ee6ea366b10
HetalRamani <hetal@qpair.io>
2017-09-11 19:29:37 -0400

    implemented date-picker and fixed all the bugs which were assigned to me including show tenant expenses By current Month on dashboard and ticket link is https://codeon.atlassian.net/browse/CONSENSUS-57

--- Consensus-frontend/src/app/pages/dashboard/feed/feed.component.ts
+++ Consensus-frontend/src/app/pages/dashboard/feed/feed.component.ts
@@ -25,7 +25,7 @@ export class Feed {
     tenantsId = [];
     list = [];
     monthYear = [];
-    tenantp = [];
+    tenantp = [];date=[];
     tenantName = [];
     ExpensesTenantList = [];
 
@@ -33,82 +33,47 @@ export class Feed {
     constructor(private _feedService: FeedService,
         private tenantService: TenantService) {
 
-        this.tenantService.getTenantsByCommunity()
-            .then(data => {
 
-                this.tenants = data;
-                var tenantLoop;
+      this.tenantService.getTenantsPayment().then(
+        data => {
 
-                for (tenantLoop = 0; tenantLoop < this.tenants.length ; tenantLoop++) {
+          this.tenantsPayment = data;
+          console.log("This is selected month"+JSON.stringify(this.tenantsPayment));
 
-                this.tenantName.push({tenantId: this.tenants[tenantLoop]._id,
-                                    firstName: this.tenants[tenantLoop].firstName});
+          if(this.tenantsPayment.length == 0){
 
+            console.log("Tenant"+ this.tenantsPayment.length);
+            this.setBoolean = false;
+          }
+          if(this.tenantsPayment.length != 0 ) {
+            this.setBoolean = true;
 
-                }
-
-
-            });
-
-
+      let currentMonth= this.isCurrentYear();
 
 
+            for (var i = 0; i < this.tenantsPayment.length; i++) {
 
-             for (var i = 0; i < this.tenantName.length ; i++) {
-
-                var data = this.tenantName[i];
-
-                   var tenantId = data.tenantId ;
-                   var  firstName =  data.firstName ;
+              if(currentMonth == this.tenantsPayment[i].myDate.date.month) {
+                this.ExpensesTenantList.push(
+                  {
 
 
+                    TenantName: this.tenantsPayment[i].tenantName,
+
+                    amountPaid: this.tenantsPayment[i].amountPaid,
 
 
-                      this.tenantService.getExpenseByTenantsPayment(tenantId,firstName)
-                        .then(data => {
+                    date: this.tenantsPayment[i].myDate.jsdate
 
 
-                            this.tenantsPayment = data;
-
-
-
-                            for (var i = 0; i < this.tenantsPayment.length ; i++) {
+                  });
+              }
+            }
+          }
 
 
 
-                                if (this.tenantsPayment[i].amountPaid == "" || this.tenantsPayment[i].amountPaid == null) {
-
-                                    this.setBoolean = false;
-
-                                } else {
-
-
-
-                                    this.setBoolean = true;
-
-                                    this.ExpensesTenantList.push({
-
-                                        amountPaid: this.tenantsPayment[i].amountPaid,
-                                        monthYear: this.tenantsPayment[i].monthYear
-
-                                    });
-
-
-
-
-                                }
-
-
-
-                            }
-
-
-
-
-                             });
-
-                      }
-
+        });
 
 
 
@@ -123,5 +88,13 @@ export class Feed {
         message.expanded = !message.expanded;
     }
 
+
+  isCurrentYear(){
+    let date = new Date();
+    let iMonth = date.getMonth()+1;
+    console.log(iMonth)
+
+    return iMonth;
+  }
 
 }
